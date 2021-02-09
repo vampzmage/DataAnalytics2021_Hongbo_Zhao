069a01cb2c591ab29c4d0727a451c19bf9f6dd9a
Vampzmage <57106304+vampzmage@users.noreply.github.com>
2021-02-09 12:31:12 -0500

    Update lab1.R

--- lab1.R
+++ lab1.R
@@ -42,3 +42,24 @@ View(EPI_data)
 attach(EPI_data)
 fix(EPI_data)
 EPI
+tf <- is.na(EPI)
+E <- EPI[!tf]
+summary(EPI)
+fivenum(EPI,na.rm=TRUE)
+stem(EPI)
+hist(EPI)
+hist(EPI, seq(30., 95., 1.0), prob=TRUE)
+lines(density(EPI,na.rm=TRUE,bw=1.))
+rug(EPI)
+plot(ecdf(EPI), do.points=FALSE, verticals=TRUE)
+par(pty="s")
+qqnorm(EPI); qqline(EPI)
+x<-seq(30,95,1)
+qqplot(qt(ppoints(250), df = 5), x, xlab = "Q-Q plot for t dsn")
+qqline(x)
+EPILand<-EPI[!Landlock]
+Eland <- EPILand[!is.na(EPILand)]
+hist(ELand)
+hist(ELand, seq(30., 95., 1.0), prob=TRUE)
+
+
