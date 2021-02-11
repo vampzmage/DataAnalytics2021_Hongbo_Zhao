e9f0cff8f5a030644295eb7136810def62ecfab3
Vampzmage <57106304+vampzmage@users.noreply.github.com>
2021-02-11 09:24:53 -0500

    Update lab1.R

--- lab1.R
+++ lab1.R
@@ -62,4 +62,22 @@ Eland <- EPILand[!is.na(EPILand)]
 hist(ELand)
 hist(ELand, seq(30., 95., 1.0), prob=TRUE)
 
-
+plot(ecdf(EPI), do.points=FALSE, verticals=TRUE)
+par(pty="s")
+qqnorm(EPI); qqline(EPI)
+x<-seq(30,95,1)
+qqplot(qt(ppoints(250), df= 5), x, xlab= "Q-Q plot for t dsn")
+qqline(x)
+plot(ecdf(EPI_data$EPI),do.points=FALSE,verticals= TRUE)
+plot(ecdf(EPI_data$EPI),do.points=TRUE,verticals= TRUE)
+par(pty="s")
+qqnorm(EPI_data$EPI)
+qqline(EPI_data$EPI)
+x <-seq(30,95,1)
+x
+x2 <-seq(30,95,2)
+x2
+x2 <-seq(30,96,2)
+x2
+qqplot(qt(ppoints(250),df=5),x, xlab= "Q-Q plot")
+qqline(x)
